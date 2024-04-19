532ba04ff134057220038a86057cb7926b1b05c0
JulianP152002 <parracardenas17@gmail.com>
2024-04-18 21:50:32 -0500

    form

---
+++ main.js
@@ -0,0 +1,8 @@
+const form = document.querySelector('form_button')
+form.addEventListener('click', () =>{
+
+    alert ("gracias por hacernos saber de tu asistencia")
+    .then(() => {window.location.reload()})
+    
+
+})
