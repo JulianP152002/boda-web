11a1c0b7b7165e263f5d44c516882ade5990e132
JulianP152002 <parracardenas17@gmail.com>
2024-04-20 14:10:06 -0500

    finish

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 const form = document.querySelector('form_button')
 form.addEventListener('click', () =>{
-
-    alert ("gracias por hacernos saber de tu asistencia")
-    .then(() => {window.location.reload()})
-    
+    alert ("Gracias por enviarnos tu respuesta")
+    .then(() => {window.location.reload()})    
 
 })
