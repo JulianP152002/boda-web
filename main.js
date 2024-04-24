8901197ece4887a8ebe46761e143dee53d23a298
JulianP152002 <parracardenas17@gmail.com>
2024-04-23 19:24:47 -0500

    fininalizado

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
-const form = document.querySelector('form_button')
-form.addEventListener('click', () =>{
-    alert ("Gracias por enviarnos tu respuesta")
-    .then(() => {window.location.reload()})    
+const scriptURL = 'https://script.google.com/macros/s/AKfycbwlx_TvNxTC21aOa5r07uDroswrcL0-4I3NpOXi5MVZZjNUI-Z3MoM_3OFSKq7vEm9_/exec'
 
+const form = document.forms['contact-form']
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+  .then(response => alert("Gracias por hacernos saber tu asistencia, alli te esperamos." ))
+  .then(() => { window.location.reload(); })
+  .catch(error => console.error('Error!', error.message))
 })
